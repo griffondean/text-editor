5ee1f27332169aa22536a2589e984f4f05eacf1a
Griffon Dean <10820413@my.uvu.edu>
2023-07-16 18:03:35 -0400

    added logic to install.js for event handlers

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,44 +12,44 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('Post to the database');
 
-  // Create a connection to the database database and version we want to use.
+  // Create a connection to database and version we want to use
   const jateDb = await openDB('jate', 1);
 
-  // Create a new transaction and specify the database and data privileges.
+  // Create a new transaction and specify the database
   const tx = jateDb.transaction('jate', 'readwrite');
 
-  // Open up the desired object store.
+  // Open up the object store
   const store = tx.objectStore('jate');
 
-  // Use the .add() method on the store and pass in the content.
+  // Use the .add() method on the store and pass in the content
   const request = store.put({ id: 1, value: content });
 
-  // Get confirmation of the request.
+  // Get confirmation of the request
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
 
-  // Create a connection to the database database and version we want to use.
+  // Create a connection to database and version we want to use
   const jateDb = await openDB('jate', 1);
 
-  // Create a new transaction and specify the database and data privileges.
+  // Create new transaction and specify the database
   const tx = jateDb.transaction('jate', 'readonly');
 
-  // Open up the desired object store.
+  // Open up the desired object store
   const store = tx.objectStore('jate');
 
-  // Use the .getAll() method to get all data in the database.
+  // Use the .getAll() method to get all the data in the database
   const request = store.getAll();
 
-  // Get confirmation of the request.
+  // Get confirmation of the request
   const result = await request;
   console.log('result.value', result);
   return result;
